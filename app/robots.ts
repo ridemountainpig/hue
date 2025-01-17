a948d1496805fd646792c2e85ab343de11c2a2c6
yen-cheng <lin0905580802@gmail.com>
2025-01-17 11:29:05 +0800

    chore: update sitemap url

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: "/",
             disallow: ["/api/", "/_next/static/media/"],
         },
-        sitemap: "https://hue-palette.com/sitemap.xml",
+        sitemap: "https://www.hue-palette.com/sitemap.xml",
     };
 }
